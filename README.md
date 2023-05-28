011eb5776eacacbbfa5e946711ada5e75a3e9fe8
Molly <mojeanmac@gmail.com>
2023-05-28 13:20:14 -0700

    Explore

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Molly MacLaren
         - Others can see the results of the tests you ran.
 2. Would you use an end to end test to check if a function is returning the correct output?
    - No, only unit testing would be necessary.
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+   - No, a message feature is too abstract and has too many moving parts. I would test individual aspects of the feature though.
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+   - Yes, it would be really easy to test with a message > 80.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('accepts valid phone 1', () => {
+    expect(functions.isPhoneNumber('123-305-4567')).toBe(true);
+});
+
+test('accepts valid phone 2', () => {
+    expect(functions.isPhoneNumber('(456) 456-7890')).toBe(true);
+});
+
+test('rejects invalid phone 1', () => {
+    expect(functions.isPhoneNumber('1234567890')).toBe(false);
+});
+
+test('rejects invalid phone 2', () => {
+    expect(functions.isPhoneNumber('(098) 987-abcd')).toBe(false);
+});
+
+test('accepts valid email 1', () => {
+    expect(functions.isEmail('admin@ucsd.edu')).toBe(true);
+});
+
+test('accepts valid email 2', () => {
+    expect(functions.isEmail('ilovecats97@yahoo.com')).toBe(true);
+});
+
+test('rejects invalid email 1', () => {
+    expect(functions.isEmail('amongus@sussy.imposter')).toBe(false);
+});
+
+test('rejects invalid email 2', () => {
+    expect(functions.isEmail('#$%^&*@us.gov')).toBe(false);
+});
+
+test('accepts valid password 1', () => {
+    expect(functions.isStrongPassword('Am0ngUs')).toBe(true);
+});
+
+test('accepts valid password 2', () => {
+    expect(functions.isStrongPassword('password')).toBe(true);
+});
+
+test('rejects invalid password 1', () => {
+    expect(functions.isStrongPassword('hi')).toBe(false);
+});
+
+test('rejects invalid password 2', () => {
+    expect(functions.isStrongPassword('SDfds j34HDbA39')).toBe(false);
+});
+
+test('accepts valid date 1', () => {
+    expect(functions.isDate('05/09/2003')).toBe(true);
+});
+
+test('accepts valid date 2', () => {
+    expect(functions.isDate('91/19/9999')).toBe(true);
+});
+
+test('rejects invalid date 1', () => {
+    expect(functions.isDate('5/9/03')).toBe(false);
+});
+
+test('rejects invalid date 2', () => {
+    expect(functions.isDate('05 / 09 / 2003')).toBe(false);
+});
+
+test('accepts valid hex color 1', () => {
+    expect(functions.isHexColor('FFF')).toBe(true);
+});
+
+test('accepts valid hex color 2', () => {
+    expect(functions.isHexColor('#3e355c')).toBe(true);
+});
+
+test('rejects invalid hex color 1', () => {
+    expect(functions.isHexColor('#000 000')).toBe(false);
+});
+
+test('rejects invalid hex color 2', () => {
+    expect(functions.isHexColor('f')).toBe(false);
+});
